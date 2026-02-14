1ac8884bfb17a7851a2634d1f49e07c385dec006
Orellano Celeste <orellanoceleste14@gmail.com>
2026-02-13 22:52:20 -0300

    Rutas de libros y productos finalizadas

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js";
 import productosRouter from "./routes/productos.js"
 import librosRouter from "./routes/libros.js"
 import "dotenv/config"
 
 const app = express()
-
+connectDB();
 app.use(cors())
 app.use(express.json())
 
 app.use("/productos", productosRouter)
 app.use("/libros", librosRouter)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`)
+  console.log(`Servidor backend corriendo en puerto: ${PORT}`)
 })
