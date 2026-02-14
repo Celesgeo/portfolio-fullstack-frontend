21eed3db01da0bbd8234dc39cbc63a684955bf64
Orellano Celeste <orellanoceleste14@gmail.com>
2026-02-13 21:02:29 -0300

    Backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,24 @@
-// backend/config/db.js
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // <--- Este es el de Aiven
-  ssl: { rejectUnauthorized: false } 
-});
+import mysql from "mysql2/promise";
+
+export let db;
+
+export const connectDB = async () => {
+  try {
+    
+    db = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,      
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
+
+    console.log("✅ Conectado a la base de datos en Aiven");
+  } catch (error) {
+    console.error("❌ Error al conectar a la base de datos:", error);
+    process.exit(1);
+  }
+};
