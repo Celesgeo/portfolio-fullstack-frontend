043b5703daa416ed53b35f6f96f6e6cf08fc20e5
Orellano Celeste <orellanoceleste14@gmail.com>
2026-02-13 22:22:49 -0300

    Fix:ssl y nombres de variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,20 @@
+// backend/config/db.js
 import mysql from "mysql2/promise";
 
 export let db;
 
 export const connectDB = async () => {
   try {
-    
     db = mysql.createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD, 
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT,      
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false } 
     });
-
-    console.log("✅ Conectado a la base de datos en Aiven");
+    console.log("✅ Conexión exitosa a Aiven");
   } catch (error) {
-    console.error("❌ Error al conectar a la base de datos:", error);
-    process.exit(1);
+    console.error("❌ Error de conexión:", error);
   }
 };
